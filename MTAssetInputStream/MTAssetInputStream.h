9ec51b355f47d489077396fd141ce1bbdc73acf0
MartinTsao <qinmin@martintsaodeimac.local>
2018-04-18 14:19:35 +0800

    Initial Commit

---
+++ MTAssetInputStream/MTAssetInputStream.h
@@ -0,0 +1,19 @@
+//
+//  MTAssetInputStream.h
+//  MTAssetInputStream
+//
+//  Created by MartinTsao on 2018/4/18.
+//  Copyright © 2018年 MartinTsao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MTAssetInputStream.
+FOUNDATION_EXPORT double MTAssetInputStreamVersionNumber;
+
+//! Project version string for MTAssetInputStream.
+FOUNDATION_EXPORT const unsigned char MTAssetInputStreamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MTAssetInputStream/PublicHeader.h>
+
+
